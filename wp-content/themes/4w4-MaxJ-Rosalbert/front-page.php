1103468ef1e6dec2534f9721ff2be8794c6fe756
Max-Joseph <e1862695@cmaisonneuve.qc.ca>
2023-03-02 15:04:28 -0500

    modification du header pour qu'il change en fonction du responsive

--- wp-content/themes/4w4-MaxJ-Rosalbert/front-page.php
+++ wp-content/themes/4w4-MaxJ-Rosalbert/front-page.php
@@ -11,11 +11,11 @@
         <section class="blocflex">
             <?php if(have_posts()):
                 while (have_posts()): the_post(); ?>
-                <?php if (in_category('galerie')) {
-                    get_template_part("template-parts/categorie", "galerie");
-                }else{
-                    get_template_part("template-parts/categorie", "notes-4w4");
-                }?>
+               <?php  if (in_category('galerie')){
+                get_template_part("template-parts/categorie","galerie");
+               } else { echo ('');
+                get_template_part("template-parts/categorie","notes-4w4");
+               }?>
                 <?php endwhile; ?>
             <?php  endif; ?>
         </section>
