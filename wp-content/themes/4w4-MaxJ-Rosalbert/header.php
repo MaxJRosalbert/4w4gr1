4d88c7292fef7d5ab8307108fd61ec93600a39a6
Max-Joseph <e1862695@cmaisonneuve.qc.ca>
2023-05-23 09:57:40 -0400

    Ef-MJR-1 Premier transfere de contenus vers siteGround

--- wp-content/themes/4w4-MaxJ-Rosalbert/header.php
+++ wp-content/themes/4w4-MaxJ-Rosalbert/header.php
@@ -43,4 +43,13 @@ if (! is_front_page() && ! is_404()) {
     get_template_part("template-parts/aside") ;
 } 
 
+$nouvelle_classe = "aside-atelier";
+if  (is_front_page() || 
+    (! in_category("cours") && ! in_category("4w4")) ){
+      $nouvelle_classe = 'no-aside';
+    }
+if (is_page_template('template-atelier.php')) {
+     $nouvelle_classe = 'y-aside';
+}
+
 ?>
---
+++ wp-content/themes/4w4-MaxJ-Rosalbert/template-atelier.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template name: Atelier
+ */
+?>
+<?php get_header(); ?>
+<main class="site__main">
+    <?php
+        if ( have_posts() ) : the_post(); ?>
+        <?php the_post_thumbnail('medium'); ?>
+        <h1><?= get_the_title(); ?></h1>
+        <?php the_content();?>
+        <p>Date <?php the_field('date'); ?></p>     
+        <p>Heure <?php the_field('heure'); ?></p>     
+        <p>Durée<?php the_field('duree'); ?></p>     
+        <p>Local<?php the_field('local'); ?></p>     
+        <p>Formateur<?php the_field('formateur'); ?></p>     
+        <?php endif;?>
+</main><!-- #main -->
+    <?php
+get_footer();
